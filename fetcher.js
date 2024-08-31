8ad588bb44c2cbb04e40afb989d1476e6a7cc5b5
Şerefsaid Nurkaya <serefsaid@msn.com>
2024-08-31 21:34:17 +0300

    Initial commit

---
+++ fetcher.js
@@ -0,0 +1,11 @@
+async function sendMessage(last_prompt,messages) {
+    const response = await fetch('http://localhost:5000/chat', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ last_prompt: last_prompt,messages:messages })
+    });
+    const data = await response.json();
+    return data.response;
+}
