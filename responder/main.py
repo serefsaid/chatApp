44f5c27f9cea42148028c9e3022abea6314cdf0e
x <serefsaid@msn.com>
2024-08-31 23:13:09 +0300

    Update main.py

--- responder/main.py
+++ responder/main.py
@@ -1,5 +1,5 @@
 import ollama
 
 def get_response(last_prompt,messages):
-    response = ollama.chat(model='sevda', messages=messages)
+    response = ollama.chat(model='dolphin-llama3', messages=messages)
     return response
